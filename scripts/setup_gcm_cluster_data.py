3e3dd4f8f0c39cc612871a54db970c66b98d564f
Jared Oyler <jaredwo@gmail.com>
2016-09-12 09:03:04 -0400

    cleanup of setup_gcm_cluster_data.py

--- scripts/setup_gcm_cluster_data.py
+++ scripts/setup_gcm_cluster_data.py
@@ -1,18 +1,27 @@
+'''
+Script to setup data for cluster analysis of projected GCM precipitation and
+temperature trends.
+'''
+
 import pandas as pd
 import numpy as np
+import esd
+import os
 
 if __name__ == '__main__':
     
-    anom_fpaths = [('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_tas_2006_2099_ann.csv','tair_ann'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_tas_2006_2099_mths10-11.csv','tair_mths10-11'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_tas_2006_2099_mths12-1-2.csv','tair_mths12-1-2'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_tas_2006_2099_mths4-5.csv','tair_mths4-5'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_tas_2006_2099_mths6-7-8-9.csv','tair_mths6-7-8-9'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_pr_2006_2099_ann.csv','prcp_ann'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_pr_2006_2099_mths10-11.csv','prcp_mths10-11'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_pr_2006_2099_mths12-1-2.csv','prcp_mths12-1-2'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_pr_2006_2099_mths4-5.csv','prcp_mths4-5'),
-                   ('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/anoms_pr_2006_2099_mths6-7-8-9.csv','prcp_mths6-7-8-9'),]
+    path_cmip5_trends = esd.cfg.path_cmip5_trends
+    
+    anom_fpaths = [(os.path.join(path_cmip5_trends, 'anoms_tas_2006_2099_ann.csv'),'tair_ann'),
+                   (os.path.join(path_cmip5_trends, 'anoms_tas_2006_2099_mths10-11.csv'),'tair_mths10-11'),
+                   (os.path.join(path_cmip5_trends, 'anoms_tas_2006_2099_mths12-1-2.csv'),'tair_mths12-1-2'),
+                   (os.path.join(path_cmip5_trends, 'anoms_tas_2006_2099_mths4-5.csv'),'tair_mths4-5'),
+                   (os.path.join(path_cmip5_trends, 'anoms_tas_2006_2099_mths6-7-8-9.csv'),'tair_mths6-7-8-9'),
+                   (os.path.join(path_cmip5_trends, 'anoms_pr_2006_2099_ann.csv'),'prcp_ann'),
+                   (os.path.join(path_cmip5_trends, 'anoms_pr_2006_2099_mths10-11.csv'),'prcp_mths10-11'),
+                   (os.path.join(path_cmip5_trends, 'anoms_pr_2006_2099_mths12-1-2.csv'),'prcp_mths12-1-2'),
+                   (os.path.join(path_cmip5_trends, 'anoms_pr_2006_2099_mths4-5.csv'),'prcp_mths4-5'),
+                   (os.path.join(path_cmip5_trends, 'anoms_pr_2006_2099_mths6-7-8-9.csv'),'prcp_mths6-7-8-9'),]
     
     anoms_all = []
     
@@ -22,6 +31,7 @@ if __name__ == '__main__':
         
         anoms = pd.read_csv(fpath, index_col='time')
         anoms.index = pd.DatetimeIndex(anoms.index)
+        # resample to 10-year means for the cluster analysis
         anoms = anoms.resample('10AS').mean()
         anoms = anoms.stack()
         anoms.index.names = ['time', 'model_name']
@@ -37,6 +47,6 @@ if __name__ == '__main__':
            str(anoms_all.index[anoms_all.isnull().sum(axis=1) > 0]))
     anoms_all = anoms_all.dropna()
     anoms_all.index.name = 'model_name'
-    anoms_all.to_csv('/storage/home/jwo118/group_store/red_river/cmip5_trends_r2/cluster_anoms.csv')
+    anoms_all.to_csv(os.path.join(esd.cfg.path_cmip5_trends, 'cluster_anoms.csv'))
     
     
