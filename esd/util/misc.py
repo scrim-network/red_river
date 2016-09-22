10ab1b86659fd75726f6ac3f45f7e23d2f6dafd0
Jared Oyler <jaredwo@gmail.com>
2016-09-22 08:57:49 -0400

    added great circle dist and spell length functions

--- esd/util/misc.py
+++ esd/util/misc.py
@@ -2,6 +2,7 @@ import os
 import errno
 import time
 import sys
+import numpy as np
 
 def mkdir_p(path):
     '''
@@ -66,3 +67,34 @@ class StatusCheck(object):
             sys.stdout.flush()
             self.status_time = time.time()
             self.num_last_check = self.num
+            
+RADIAN_CONVERSION_FACTOR = 0.017453292519943295 #pi/180
+AVG_EARTH_RADIUS_KM = 6371.009 #Mean earth radius as defined by IUGG
+
+def grt_circle_dist(lon1,lat1,lon2,lat2):
+    '''
+    Calculate great circle distance according to the haversine formula
+    see http://en.wikipedia.org/wiki/Great-circle_distance
+    '''
+    #convert to radians
+    lat1rad = lat1 * RADIAN_CONVERSION_FACTOR
+    lat2rad = lat2 * RADIAN_CONVERSION_FACTOR
+    lon1rad = lon1 * RADIAN_CONVERSION_FACTOR
+    lon2rad = lon2 * RADIAN_CONVERSION_FACTOR
+    deltaLat = lat1rad - lat2rad
+    deltaLon = lon1rad - lon2rad
+    centralangle = 2 * np.arcsin(np.sqrt((np.sin (deltaLat/2))**2 + np.cos(lat1rad) * np.cos(lat2rad) * (np.sin(deltaLon/2))**2))
+    #average radius of earth times central angle, result in kilometers
+    #distDeg = centralangle/RADIAN_CONVERSION_FACTOR
+    distKm = AVG_EARTH_RADIUS_KM * centralangle 
+    return distKm
+    
+def runs_of_ones_array(bits):
+    # http://stackoverflow.com/questions/1066758/find-length-of-sequences-of-identical-values-in-a-numpy-array
+    # make sure all runs of ones are well-bounded
+    bounded = np.hstack(([0], bits, [0]))
+    # get 1 at run starts and -1 at run ends
+    difs = np.diff(bounded)
+    run_starts, = np.where(difs > 0)
+    run_ends, = np.where(difs < 0)
+    return run_ends - run_starts
