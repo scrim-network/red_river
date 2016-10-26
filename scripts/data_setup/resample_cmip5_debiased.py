03de79f66e4d25984551a68050aee02d0ece8281
Jared Oyler <jaredwo@gmail.com>
2016-10-26 11:13:01 -0400

    remove with_grid_attrs subdir from input path

--- scripts/data_setup/resample_cmip5_debiased.py
+++ scripts/data_setup/resample_cmip5_debiased.py
@@ -15,18 +15,13 @@ if __name__ == '__main__':
     # Upsample 1deg debiased CMIP5 datasets to Red River 0.25deg Aphrodite grid with
     # bicubic or bilinear interpolation (remapbil or remapbic)
     # These are used in the analog downscaling procedure
-    path_in = os.path.join(esd.cfg.path_cmip5_debiased, 'with_grid_attrs')
+    path_in = os.path.join(esd.cfg.path_cmip5_debiased)
     resample_method = 'remapbic' #remapbil or remapbic
     path_out = os.path.join(esd.cfg.path_cmip5_debiased, 'resampled')
     mkdir_p(path_out)
     
     fpath_cdo_grid_def = os.path.join(esd.cfg.path_data_bbox, 'cdo_grid_p25deg_red_river')
-    
-    def upsample_cdo(fpath_in, fpath_out, method):
-        cmd = "cdo %s,%s %s %s"%(method, fpath_cdo_grid_def, fpath_in, fpath_out)
-        print cmd
-        subprocess.call(cmd, shell=True)
-        
+            
     paths_rcp = sorted(glob.glob(os.path.join(path_in, "historical*_merged")))
     
     for path_rcp in paths_rcp:
